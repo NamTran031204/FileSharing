c76a8c5a1c25f3f95547eab718e20ab57627449a
Tran Nam <tpnam1w3@gmail.com>
2026-04-16 00:01:45 +0700

    router config

--- client/src/utils/RouterConfigUtil.tsx
+++ client/src/utils/RouterConfigUtil.tsx
@@ -1,45 +1,118 @@
 import type { ReactNode } from 'react';
-import LoginPage from "../page/Phase1/auth/LoginPage.tsx";
-import RegisterPage from "../page/Phase1/auth/RegisterPage.tsx";
-import UserFilePage from "../page/Phase1/userFilePage";
-import UploadPage from "../page/Phase1/uploadPage";
-import UserProfilePage from "../page/Phase1/UserProfilePage/UserProfilePage.tsx";
-import TrashPage from "../page/Phase1/trashPage";
-import ImageReviewPage from "../page/ImageReviewPage.tsx";
-import ImageReviewV2 from "../page/ImageReviewV2.tsx";
-import LocalImageCanvas from "../mockup/pages/KonvaDemo.tsx";
+import type {RouteObject} from "react-router-dom";
+import LoginPage from '../page/Phase1/auth/LoginPage';
+import RegisterPage from '../page/Phase1/auth/RegisterPage';
+import UserFilePage from '../page/Phase1/userFilePage';
+import UploadPage from '../page/Phase1/uploadPage';
+import UserProfilePage from '../page/Phase1/UserProfilePage/UserProfilePage.tsx';
+import TrashPage from '../page/Phase1/trashPage';
+import FilePreviewPage from '../page/Phase1/filePreviewPage';
+import ImageReviewPage from '../page/ImageReviewPage';
+import ImageReviewV2 from '../page/ImageReviewV2';
+import LoginPageV2 from '../page/LoginPageV2.tsx';
+import MainPage from '../page/MainPage';
+import LocalImageCanvas from '../mockup/pages/KonvaDemo.tsx';
+import HlsVideoMockup from '../mockup/pages/HlsVideoMockup.tsx';
+import ProjectMain from "../page/ProjectMain.tsx";
 
-export interface AppRoute {
+export interface AppRoute extends Omit<RouteObject, 'children'>{
     path?: string;
-    index?: boolean;
-    element?: ReactNode;
-    redirectTo?: string;
-    isProtected?: boolean;
+    name?: string;
+    redirectTo?: string; // không truyền gì mặc định = path
+    element?: ReactNode; // react component
+    icon?: ReactNode; // antd icon
+    isShow?: boolean; // dùng sau
+    roles?: string[];
+    isProtected?: boolean; // để đóng gói vào ProtectedRoute
     children?: AppRoute[];
 }
 
 export const ROUTER_CONFIG: AppRoute[] = [
     {
         path: '/',
-        redirectTo: '/login'
+        redirectTo: '/home',
+        isShow: false,
     },
     {
         path: '/login',
-        element: <LoginPage />
+        name: 'Login',
+        element: <LoginPage/>,
+        isShow: false,
+    },
+    {
+        path: '/loginv2',
+        name: 'Login V2',
+        element: <LoginPageV2/>,
+        isShow: false,
     },
     {
         path: '/register',
-        element: <RegisterPage />
+        name: 'Register',
+        element: <RegisterPage/>,
+        isShow: false,
+    },
+    {
+        path: '/home',
+        name: 'Home',
+        element: <MainPage/>,
+    },
+    {
+        path: '/preview/*',
+        name: 'Preview',
+        element: <FilePreviewPage/>,
     },
     {
         path: '/my-files',
-        element: <UserFilePage />,
-        isProtected: true
+        name: 'My Files',
+        element: <UserFilePage/>,
+        isProtected: true,
     },
-    { path: '/upload', element: <UploadPage />, isProtected: true },
-    { path: '/profile', element: <UserProfilePage />, isProtected: true },
-    { path: '/trash', element: <TrashPage />, isProtected: true },
-    { path: '/review/image', element: <ImageReviewPage />, isProtected: true },
-    { path: '/review/image-v2', element: <ImageReviewV2 />, isProtected: true },
-    { path: '/mockup/image-review', element: <LocalImageCanvas /> },
+    {
+        path: '/upload',
+        name: 'Upload',
+        element: <UploadPage/>,
+        isProtected: true,
+    },
+    {
+        path: '/profile',
+        name: 'Profile',
+        element: <UserProfilePage/>,
+        isProtected: true,
+    },
+    {
+        path: '/trash',
+        name: 'Trash',
+        element: <TrashPage/>,
+        isProtected: true,
+    },
+    {
+        path: '/review/image',
+        name: 'Image Review',
+        element: <ImageReviewPage/>,
+        isProtected: true,
+    },
+    {
+        path: '/mockup/image-review',
+        name: 'Mockup Image Review',
+        element: <LocalImageCanvas/>,
+        isShow: false,
+    },
+    {
+        path: '/review/image-v2',
+        name: 'Image Review V2',
+        element: <ImageReviewV2/>,
+        isShow: false,
+    },
+    {
+        path: '/projects',
+        name: 'Projects',
+        element: <ProjectMain/>,
+        isShow: true,
+    },
+    {
+        path: '/mockup/hls-video',
+        name: 'Mockup HLS Video',
+        element: <HlsVideoMockup/>,
+        isShow: false,
+    },
 ];
